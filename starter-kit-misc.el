1aae6bbba02b9c136e00d26a88fd976826cb1770
J.A. Roberts Tunney <jtunney@lobstertech.com>
2009-09-11 11:41:30 -0400

    forget emacs 21

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -74,13 +74,12 @@
 (delete-selection-mode t)
 
 ;; ido-mode is like magic pixie dust!
-(when (> emacs-major-version 21)
-  (ido-mode t)
-  (setq ido-enable-prefix nil
-        ido-enable-flex-matching t
-        ido-create-new-buffer 'always
-        ido-use-filename-at-point t
-        ido-max-prospects 10))
+(ido-mode t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point t
+      ido-max-prospects 10)
 
 (set-default 'indent-tabs-mode nil)
 (set-default 'indicate-empty-lines t)
