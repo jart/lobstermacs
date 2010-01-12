3ceb0186eb73aeca316669916d38f8b94a23e2fc
J.A. Roberts Tunney <jart@worky.(none)>
2010-01-12 04:30:55 -0500

    In Python: press F10 to run your script in Pymacs

--- lobstermacs-python.el
+++ lobstermacs-python.el
@@ -7,6 +7,7 @@
   '(progn
      (define-key python-mode-map (kbd "<return>") 'newline-and-indent)
      (define-key python-mode-map (kbd "C-x C-e") 'lob/python-eval)
+     (define-key python-mode-map [f10] 'lob/python-run)
      (define-key lisp-mode-shared-map (kbd "C-c l") "lambda")
      ;; (add-hook 'python-mode-hook 'idle-highlight)
      (add-hook 'python-mode-hook 'run-coding-hook)))
@@ -24,6 +25,9 @@ Where the code comes from, ordered by priority:
 
 This provides a simpler user interface to the `pymacs-eval' and
 `pymacs-exec' functions.
+
+Any variables you set or imports you make will persist between
+requests (unless you call `pymacs-terminate-services'.)
 "
   (interactive)
   (if (not (fboundp 'textwrap-dedent))
@@ -32,38 +36,60 @@ This provides a simpler user interface to the `pymacs-eval' and
                   (lob/highlighted-text)
                   (lob/current-python-statement)
                   (read-string "Python expression? "))))
-    (let ((result (if (lob/is-python-statement code)
-                      (pymacs-exec (textwrap-dedent code))
-                    (pymacs-eval code))))
+    (let ((result (if (lob/is-python-expression code)
+                      (pymacs-eval code)
+                    (pymacs-exec (textwrap-dedent code)))))
       (if (interactive-p)
           (message "%S" result))
       result)))
 
 
-(defun lob/is-python-statement (code)
-  "Determine if Python code should be sent to exec()
+(defun lob/python-run (&optional filepath)
+  "Loads and Executes Python Script
+
+This is essentially the same as highlighting your entire buffer
+and running `lob/python-eval'.  The only differences are:
+
+ * This will execute \"if __name__ == '__main__':\" code
+ * It's less noisy in the `*Pymacs*' buffer (also faster)
+"
+  (interactive)
+  (let ((path (or filepath
+                  (if (or (string-match "\\.py$" (buffer-file-name))
+                          (equal major-mode 'python-mode))
+                      (progn
+                        (if (buffer-modified-p)
+                            (save-buffer))
+                        (buffer-file-name)))
+                  (ido-read-file-name "Python Script? "))))
+    (pymacs-exec "__name__ = '__main__'")
+    (unwind-protect
+        (pymacs-exec (format "exec(open(%S).read())" path))
+      (pymacs-exec "__name__ = 'Pymacs.pymacs'"))))
+
+
+(defun lob/is-python-expression (code)
+  "Determine if Python code can be sent to eval()
 
 Returns true if CODE matches following criteria:
 
+  * Doesn't have an assingment operator
   * Consists of a single statement or expression
-  * Doesn't have any block statements like import, if, etc.  
-
-This makes life simpler for the user because they don't have to
-figure our each time they want to evaluate code if it should be
-sent to exec() or eval().
+  * Doesn't have any block statements like import, if, etc.
 "
   (and
+   (not (string-match "[ a-zA-Z0-9]=[ a-zA-Z\\]" code))
    (with-temp-buffer
      (insert code)
      (beginning-of-buffer)
      (python-end-of-statement)
-     (not (null (search-forward-regexp "[\^ \t\r\n]" nil t))))
+     (null (search-forward-regexp "[\^ \t\r\n]" nil t)))
    (let ((keywords '(del while elif global with assert else if pass
                      yield import from print class raise try except
                      finally break continue return def for)))
      (let ((regex (mapconcat 'symbol-name keywords "\\|")))
-       (not (null (string-match (concat "\n[ \t\r\n]*\\(" regex "\\)[\^a-za-z]")
-                                (concat "\n" code "\n"))))))))
+       (null (string-match (concat "\n[ \t\r\n]*\\(" regex "\\)[\^a-za-z]")
+                           (concat "\n" code "\n")))))))
 
 
 (defun lob/current-python-statement ()
