9fa96b3d00f5fb4617847c07d0f86a04f6e86afa
J.A. Roberts Tunney <jart@worky.(none)>
2010-01-19 05:39:38 -0500

    whoops forgot to run the color theme lol

--- init.el
+++ init.el
@@ -44,8 +44,9 @@
 (require 'lobstermacs-defuns)
 (require 'lobstermacs-system)
 ;; enable zenburn theme if we have 256+ colors
-(if (and lob/is-colorful (not (featurep 'zenburn)))
-  (require 'zenburn))
+(when (and lob/is-colorful (not (featurep 'zenburn)))
+  (require 'zenburn)
+  (color-theme-zenburn))
 
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
