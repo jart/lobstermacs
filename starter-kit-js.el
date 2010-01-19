518f525c32951a0f9f082b5d94052d579b92b05e
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-19 10:14:31 -0500

    turn paredit back on for espresso

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -12,18 +12,18 @@
 (setq espresso-indent-level 2)
 
 (eval-after-load 'espresso
-  '(progn ;; (define-key espresso-mode-map "{" 'paredit-open-curly)
-          ;; (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
+  '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
+          (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
           ;; fixes problem with pretty function font-lock
           (define-key espresso-mode-map (kbd ",") 'self-insert-command)
           (font-lock-add-keywords 'espresso-mode
                         '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
                            1 font-lock-warning-face t)))
-          ;; (font-lock-add-keywords
-          ;;  'espresso-mode `(("\\(function *\\)("
-          ;;                    (0 (progn (compose-region (match-beginning 1)
-          ;;                                              (match-end 1) "ƒ")
-          ;;                              nil)))))
+          (font-lock-add-keywords
+           'espresso-mode `(("\\(function *\\)("
+                             (0 (progn (compose-region (match-beginning 1)
+                                                       (match-end 1) "ƒ")
+                                       nil)))))
           ))
 
 (provide 'starter-kit-js)
