7694975364eee21835c5d7a882df4317c2c26908
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-01-19 11:08:07 -0500

    Make paredit actually work for espresso javaSCRIPT

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -6,7 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
 (add-hook 'espresso-mode-hook 'moz-minor-mode)
-;;(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
+(add-hook 'espresso-mode-hook 'esk-paredit-nonlisp)
 (add-hook 'espresso-mode-hook 'run-coding-hook)
 (add-hook 'espresso-mode-hook 'idle-highlight)
 (setq espresso-indent-level 2)
@@ -15,7 +15,8 @@
 ;; (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 
 (eval-after-load 'espresso
-  '(progn (define-key espresso-mode-map "{" 'paredit-open-curly)
+  '(progn (load "paredit")
+          (define-key espresso-mode-map "{" 'paredit-open-curly)
           (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
           ;; fixes problem with pretty function font-lock
           (define-key espresso-mode-map (kbd ",") 'self-insert-command)
@@ -23,8 +24,7 @@
            'espresso-mode `(("\\(function *\\)("
                              (0 (progn (compose-region (match-beginning 1)
                                                        (match-end 1) "ƒ")
-                                       nil)))))
-          ))
+                                       nil)))))))
 
 (provide 'starter-kit-js)
 ;;; starter-kit-js.el ends here
