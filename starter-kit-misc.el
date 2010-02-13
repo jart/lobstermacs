f392ea914959196d5517e9700b27c58f0b8854f8
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-02-12 23:32:52 -0500

    comment out starter kit default space indent thing

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -65,7 +65,7 @@
         ido-use-filename-at-point 'guess
         ido-max-prospects 10))
 
-(set-default 'indent-tabs-mode nil)
+;; (set-default 'indent-tabs-mode nil)
 (set-default 'indicate-empty-lines t)
 (set-default 'imenu-auto-rescan t)
 
