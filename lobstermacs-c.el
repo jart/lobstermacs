1a9f634a78a43edd06c6faff4987ed7999de0b56
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-02-14 22:08:56 -0500

    Improved C/C++/Java/etc.  Made a less annoying electric newline thingy

--- lobstermacs-c.el
+++ lobstermacs-c.el
@@ -1,15 +1,81 @@
 ;;; lobstermacs-c.el
 ;;
-;; Part of Lobstermacs
+;; In this file we'll try to make programming languages with curly
+;; braces more fun to use
+;;
 
-;; change default style for curly brace langs from 'gnu' to 'linux'
-(setq c-default-style
-      (cons (cons 'other "linux")
-            (assq-delete-all 'other c-default-style)))
+(defun lob/on-c-closing-brace ()
+  (interactive)
+  (insert "}")
+  (call-interactively 'c-indent-line-or-region))
 
-(eval-after-load 'cc-mode
+(defun lob/on-c-mode-common-hook ()
+  "Sets lobstermacs default settings for curly-braced languages"
+
+  ;; don't make me press tab for every darn line
+  (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+  ;; goto next line when i press `{`
+  (c-toggle-auto-newline 1)
+  (setq-default c-electric-pound-behavior '(alignleft))
+  (define-key c-mode-base-map (kbd "}") 'lob/on-c-closing-brace)
+  (define-key c-mode-base-map (kbd ";") 'self-insert-command)
+  (setq c-hanging-semi&comma-criteria
+	'(c-semi&comma-no-newlines-before-nonblanks
+	  c-semi&comma-inside-parenlist))
+
+  ;; happy keyboard things
+  (define-key c-mode-base-map (kbd "C-M-h") 'backward-kill-word))
+
+(eval-after-load 'cc-vars
   '(progn
-     (define-key c-mode-map (kbd "C-M-h") 'backward-kill-word)
-     (define-key c-mode-map (kbd "<return>") 'newline-and-indent)))
+     (add-to-list 'c-mode-common-hook 'lob/on-c-mode-common-hook)
+
+     ;; Set default styles
+     ;;
+     ;; "linux" styling is my personal favorite.  it makes a saner default
+     ;; than "gnu" because it won't rain on your parade if you like to put
+     ;; opening brackets on their own line.
+     ;;
+     ;; http://www.chris-lott.org/resources/cstyle/LinuxKernelCodingStyle.txt
+     (setq-default c-default-style
+                   (list (cons 'c-mode "linux")
+                         (cons 'c++-mode "stroustrup")
+                         (cons 'java-mode "java")
+                         (cons 'awk-mode "awk")
+                         (cons 'other "linux")))
+
+     ;; add some more styles with intuitive names
+     (loop for offset in (list 2 4 6 8)
+           do (progn
+                (c-add-style (format "indent-tabs-%d" offset)
+                             `("linux"
+                               (indent-tabs-mode . t)
+                               (c-basic-offset . ,offset)))
+                (c-add-style (format "indent-spaces-%d" offset)
+                             `("linux"
+                               (indent-tabs-mode . nil)
+                               (c-basic-offset . ,offset)))))
+
+     (c-add-style "microsoft"
+                  '("stroustrup"
+                    (c-offsets-alist
+                     (innamespace . -)
+                     (inline-open . 0)
+                     (inher-cont . c-lineup-multi-inher)
+                     (arglist-cont-nonempty . +)
+                     (template-args-cont . +))))
+
+     (c-add-style "openbsd"
+                  '("bsd"
+                    (indent-tabs-mode . t)
+                    (defun-block-intro . 8)
+                    (statement-block-intro . 8)
+                    (statement-case-intro . 8)
+                    (substatement-open . 4)
+                    (substatement . 8)
+                    (arglist-cont-nonempty . 4)
+                    (inclass . 8)
+                    (knr-argdecl-intro . 8)))))
 
 (provide 'lobstermacs-c)
