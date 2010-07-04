d4469c3174b475dc81b764046734f490fbe4a67c
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-07-04 08:18:30 -0400

    .asc -> java-mode (why did i do this?)

--- lobstermacs-misc.el
+++ lobstermacs-misc.el
@@ -65,6 +65,7 @@
 ;; Some additional file extensions
 (add-to-list 'auto-mode-alist '("\\.xslt$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.as$" . java-mode))
+(add-to-list 'auto-mode-alist '("\\.asc$" . java-mode))
 (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
 
 ;; tiny scroll bars in minibuffer is silly
