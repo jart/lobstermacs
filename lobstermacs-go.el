aa8da98a070d1b430cc0707e71c7616a2bff98db
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-07-16 20:48:03 -0400

    C-c C-a in go-mode buffer shows assembly under cursor

--- lobstermacs-go.el
+++ lobstermacs-go.el
@@ -3,9 +3,52 @@
 ;; Customizations for Google's Go Programming Language
 ;;
 
+(defcustom go-outbuf-asm "*go-assembly*"
+  "Output assembly to this buffer when I run \\[lob/go-show-asm]"
+  :group 'go
+  :type 'string)
+
 (defun lob/go-mode-hook ()
   (make-local-variable 'tab-width)
-  (setq-default tab-width 4))
+  (setq tab-width 4))
+
+(defun lob/go-show-asm ()
+  "I show you what the Go code is your current buffer looks like
+compiled to assembly."
+  (interactive)
+  ;; save current position so we can jump to a different frame
+  (let* ((sauce buffer-file-name)
+         (curline (line-number-at-pos))
+         (oldbuf (current-buffer))
+         (olddbrf display-buffer-reuse-frames))
+    ;; so emacs doesn't create a new frame every damn time
+    (setq display-buffer-reuse-frames t)
+    (unwind-protect
+        (progn
+          ;; if buffer is currently visible, go to that frame;
+          ;; otherwise, create a new frame for asm output buffer
+          (switch-to-buffer-other-frame go-outbuf-asm)
+          ;; run compiler, dumping output to our buffer
+          (if (equal 0 (shell-command (format "6g -S %s" sauce) go-outbuf-asm))
+              ;; command succeeded, let's highlight asm for curline
+              (progn
+                ;; i know what file i'm editing thx
+                (save-excursion (replace-string (concat sauce ":") "line "))
+                ;; move cursor to anything referencing curline
+                (while (search-forward (format "line %d)" curline) nil t)
+                  (beginning-of-line)
+                  ;; remove the line and then put it back with a
+                  ;; different background face (ediff-current-diff-A)
+                  (let ((line (buffer-substring-no-properties
+                               (point) (+ 1 (line-end-position)))))
+                    (delete-region (point) (+ 1 (line-end-position)))
+                    (insert (propertize line 'face 'ediff-current-diff-A)))
+                  ;; make sure it's vertically centered
+                  (recenter))))
+          ;; go back to where we were before
+          (switch-to-buffer-other-frame oldbuf))
+      ;; restore that setting we overwrote, even if we crashed
+      (setq display-buffer-reuse-frames olddbrf))))
 
 (autoload (quote go-mode) "go-mode" "\
 Major mode for editing Go source text.
@@ -23,6 +66,7 @@ functions, and some types.  It also provides indentation that is
      (define-key go-mode-map (kbd "RET") 'newline-and-indent)
      (define-key go-mode-map (kbd "<return>") 'newline-and-indent)
      (define-key go-mode-map (kbd "C-<return>") 'c-indent-new-comment-line)
+     (define-key go-mode-map (kbd "C-c C-a") 'lob/go-show-asm)
      (add-hook 'go-mode-hook 'lob/go-mode-hook)
      ))
 
